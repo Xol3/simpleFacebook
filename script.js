e12dba1130d97542580f22144c61a3bdf53e874a
Xol3 <Xoalxiyetl@gmail.com>
2018-08-20 10:35:48 -0500

    made changes to the js

---
+++ script.js
@@ -0,0 +1,24 @@
+var database = [
+  {
+    userName: "pancho",
+    password: "poblano"
+  }
+];
+
+var newsFeed = [
+  {
+    userName: "Virgan",
+    timeline: "saquen los qintoniles"
+  },
+  {
+    userName: "Leo",
+    timeline: "Miren mis servilletas"
+  }
+]
+
+var userNamePrompt = prompt("userName");
+var passwordPrompt = prompt("password");
+
+function signIn(username, password){
+  
+}
